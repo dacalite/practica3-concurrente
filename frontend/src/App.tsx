7467b34017d2de4a63a0a7ed0d9116098f685a27
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-23 11:11:34 +0200

    init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='w-screen h-screen m-0 p-0 flex flex-col justify-center items-center'>
+      Project template
+    </div>
+  )
+}
+
+export default App
