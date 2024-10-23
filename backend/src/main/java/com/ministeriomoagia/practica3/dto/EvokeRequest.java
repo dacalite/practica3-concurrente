8e5ab5e68f5950563df98f4f79dac9a372491001
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-23 23:31:24 +0200

    feat: evoque request DTO

---
+++ backend/src/main/java/com/ministeriomoagia/practica3/dto/EvokeRequest.java
@@ -0,0 +1,11 @@
+package com.ministeriomoagia.practica3.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EvokeRequest {
+    private String magician;
+    private String spell;
+}
