78406c598e3e19db3b866036207722fc9adbf08d
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-23 23:50:33 +0200

    fix: logs service save logs in database

--- backend/src/main/java/com/ministeriomoagia/practica3/services/LoggingService.java
+++ backend/src/main/java/com/ministeriomoagia/practica3/services/LoggingService.java
@@ -1,19 +1,31 @@
 package com.ministeriomoagia.practica3.services;
 
+import com.ministeriomoagia.practica3.models.LogEntry;
+import com.ministeriomoagia.practica3.repositories.LogEntryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoggingService {
     private static final Logger logger = LoggerFactory.getLogger(LoggingService.class);
 
+    @Autowired
+    private LogEntryRepository logEntryRepository;
+
     public void logEvent(String event) {
-        logger.info("Event: " + event);
+        logger.info("Event: {}", event);
+        saveLog(event, "INFO");
     }
 
     public void logError(String error) {
-        logger.error("Error: " + error);
+        logger.error("Error: {}", error);
+        saveLog(error, "ERROR");
+    }
+
+    private void saveLog(String message, String level) {
+        LogEntry logEntry = new LogEntry(message, level);
+        logEntryRepository.save(logEntry);
     }
 }
-
