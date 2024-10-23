7685104586f5626fe1d3e0ac293b8108412e1e89
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-23 23:31:45 +0200

    feat: custom exceptions for spells evoking

---
+++ backend/src/main/java/com/ministeriomoagia/practica3/exceptions/InsufficientLevelException.java
@@ -0,0 +1,7 @@
+package com.ministeriomoagia.practica3.exceptions;
+
+public class InsufficientLevelException extends RuntimeException {
+    public InsufficientLevelException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/ministeriomoagia/practica3/exceptions/SpellTypeMismatchException.java
@@ -0,0 +1,7 @@
+package com.ministeriomoagia.practica3.exceptions;
+
+public class SpellTypeMismatchException extends RuntimeException {
+    public SpellTypeMismatchException(String message) {
+        super(message);
+    }
+}
