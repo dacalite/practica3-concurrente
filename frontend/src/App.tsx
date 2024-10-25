dcaba1027d85efcee85b05f725212df80dc434d8
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-25 21:16:36 +0200

    feat: main app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,43 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './hocs/AppRoutes'
+import { checkToken, isTokenExpired } from './lib/auth'
+
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  // Verifica si hay una sesión guardada en el localStorage
+  useEffect(() => {
+    const storedToken = localStorage.getItem('jwt_token')
+    if (storedToken) {
+      if (isTokenExpired(storedToken)) {
+        logoutUser()
+      } else {
+        checkToken(storedToken).then((isCorrectToken) =>
+          setIsAuthenticated(isCorrectToken)
+        )
+      }
+    }
+  }, [])
+
+  const logoutUser = () => {
+    setIsAuthenticated(false)
+    localStorage.removeItem('jwt_token') // Eliminar token del localStorage
+  }
+
+  const authSession = (token: string) => {
+    setIsAuthenticated(true)
+    localStorage.setItem('jwt_token', token) // Guardar token en localStorage
+  }
+
   return (
-    <div className='w-screen h-screen m-0 p-0 flex flex-col justify-center items-center'>
-      Project template
-    </div>
+    <Router>
+      <AppRoutes
+        isAuthenticated={isAuthenticated} //Para gestionar las rutas protegidas
+        authSession={authSession} //Para llamar desde la ruta de login
+        logoutUser={logoutUser} //Para llamar desde cualquier ruta
+      />
+    </Router>
   )
 }
 
